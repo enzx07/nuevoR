2b0d1e29f13b937b02e45c90994faf46d262cbc1
enzo <enzoz3929@gmail.com>
2022-08-17 19:42:33 -0300

    0101

---
+++ script.js
@@ -0,0 +1,5 @@
+let papel = document.querySelector("#papel");
+let piedra = document.querySelector("#piedra");
+let tijera = document.querySelector("#tijera");
+let compu = Math.floor(Math.random() * 3);
+let btnchequear = document.querySelector(".btnchequear");
